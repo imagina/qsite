3f85859f438a9b596f7287055bdba522ce7242e1
leo-rp <leorock1988@hotmail.com>
2024-04-29 22:48:21 -0500

    ++ inital template

---
+++ _components/master/dateRangePicker/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="pageId">
+    <div class="q-pa-md" style="max-width: 300px">
+      <q-input filled v-model="date">
+        <template v-slot:prepend>
+          <q-icon name="event" class="cursor-pointer">
+            <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+              <q-date v-model="date" mask="YYYY-MM-DD HH:mm">
+                <div class="row items-center justify-end">
+                  <q-btn v-close-popup label="Close" color="primary" flat></q-btn>
+                </div>
+              </q-date>
+            </q-popup-proxy>
+          </q-icon>
+        </template>
+        <template v-slot:append>
+          <i class="fa-light fa-filter-list"></i>
+          <!--q-icon name="access_time" class="cursor-pointer">
+            <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+              <q-time v-model="date" mask="YYYY-MM-DD HH:mm" format24h>
+                <div class="row items-center justify-end">
+                  <q-btn v-close-popup label="Close" color="primary" flat></q-btn>
+                </div>
+              </q-time>
+            </q-popup-proxy>
+          </q-icon>
+        -->
+        </template>
+      </q-input>
+    </div>
+  </div>  
+</template>
+<script lang="ts">
+import {defineComponent} from 'vue'
+import controller from 'modules/qsite/_components/master/dateRangePicker'
+
+export default defineComponent({
+  props: {
+    modelValue: { default: false}
+  },
+  emits:['update:modelValue'],
+  components: {},
+  setup(props, {emit}) {
+    return controller(props, emit)
+  }
+})
+</script>
+<style lang="scss">
+</style>
