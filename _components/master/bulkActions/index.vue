e7ec8ee3cb122f6e9174427fc5d00e038fa8eb22
SneiderDeveloper <37787288+SneiderDeveloper@users.noreply.github.com>
2024-05-10 18:37:07 -0500

    Refactor styles in bulkActions/index.vue

--- _components/master/bulkActions/index.vue
+++ _components/master/bulkActions/index.vue
@@ -175,7 +175,7 @@ export default {
     }
 }
 </script>
-<style lang="scss" scoped>
+<style>
     .alert {
         background-color: var(--q-color-warning) + '1A';
         border-color: var(--q-color-warning);
