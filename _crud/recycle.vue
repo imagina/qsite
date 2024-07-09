120a3aeafb373540c90f82b8ed05ce7367cedbab
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2024-07-09 17:30:29 -0500

    + modal

--- _crud/recycle.vue
+++ _crud/recycle.vue
@@ -2,7 +2,7 @@
   <div>
     <master-modal 
       v-model="modal.show"
-      :title="this.isRestore ? `${this.restore.title} - ${this.itemLabel}` : `${this.deletePermanently.title} - ${this.itemLabel}`" 
+      :title="`${this.itemLabel}`" 
       :loading="modal.loading"
     >
       <div>
@@ -12,6 +12,20 @@
         <dynamic-field :field="this.restore" /> 
         <dynamic-field :field="this.deletePermanently" /> 
       </div>
+      <q-separator />
+      <div class="row justify-center q-gutter-md q-my-sm">
+        <q-btn 
+          unelevated 
+          rounded 
+          label="Cancel" 
+          color="grey-4"
+          textColor="black"
+          no-caps
+          size="md"
+          @click="modal.show = false"
+        />
+      </div>
+
     </master-modal>    
     <!-- banner info -->
     <dynamic-field :field="info" />
@@ -42,7 +56,7 @@ data() {
         icon: 'fas fa-exclamation-triangle',
         message: 'This is the recycle bin, When you delete a register, it goes to this Recycle Bin as a temporary file. You can restore It or delete It permanently from its options.',        
       }
-    },
+    },    
     restore: {
       title: 'Restore register',
       type: 'banner',
@@ -50,8 +64,7 @@ data() {
         color: 'green',
         icon: 'fas fa-exclamation-triangle',
         message: 'Do you want to restore this register?, this action will move the register to its original table',
-        actions: [                    
-          
+        actions: [
           {
             props: {
               label: 'Restore this register',
@@ -71,19 +84,7 @@ data() {
         color: 'negative',
         icon: 'fas fa-exclamation-triangle',
         message: 'Do you want to delete Permanently this register?, Once you delete this register, there is no going back. Please be certain.',
-        actions: [
-          {
-            props: {
-              label: 'cancel',
-              color: 'grey-4', 
-              textColor: 'black',
-              class: 'q-mx-md'
-
-            },
-            action: () => {
-              this.modal.show = false
-            }
-          },
+        actions: [          
           {
             props: {
               label: 'Delete this register permanently',
@@ -116,15 +117,12 @@ computed: {
     } else {
       return false
     }
-  },
-  isRestore(){
-    return this.action == 'restore'
-  },
+  },  
   isRecycle(){
     return this.$route.name == 'app.recycle'
   },
   itemLabel(){    
-    return this.item ? this.item.name || this.item.title || this.item.userName || '' : ''
+    return this.item ? this.item.name || this.item.title || this.item.userName || this.item.first_name || '' : ''
   },
   getModule(){        
     const params = decodeURI(window.location).split('?')
@@ -148,12 +146,13 @@ computed: {
      const dynamicCrudData = this.$refs.componentCrudData.crudData
       //exclude page actions 
       dynamicCrudData.read['excludeActions'] =  ['new', 'edit', 'destroy', 'sync', 'export', 'share', 'recycle'] 
+      dynamicCrudData.read['excludeColumns'] = ['qr', 'status']
       // add new actions
       dynamicCrudData.read['actions'] = [
         {//restore item action
           icon: 'fa-light fa-floppy-disk-circle-arrow-right',
           color: 'green',
-          label: 'Restore item',
+          label: 'Manage register',
           //vIf: ,
           action: (item) => {
             this.item = item
@@ -161,19 +160,7 @@ computed: {
             this.modal.show = true
             this.$emit('restore', item)
           }
-        },
-        {//restore item action
-          icon: 'fa-light fa-trash-can-xmark',
-          color: 'green',
-          label: 'Delete permanently',
-          //vIf: ,
-          action: (item) => {
-            this.item = item
-            this.action = 'delete'
-            this.modal.show = true          
-            this.$emit('deletePermanently', item)
-          }
-        },
+        },        
       ]
       return dynamicCrudData
     }
