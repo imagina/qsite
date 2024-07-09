7a98475bba5b4f02eb56eaf1aed3d56daa76d4e5
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2024-07-08 19:08:28 -0500

    + added modal for restore and delete

--- _crud/recycle.vue
+++ _crud/recycle.vue
@@ -1,6 +1,22 @@
 <template>
-  <div>    
-      <component v-if="dynamicCrud" :is="dynamicCrud" ref="componentCrudData" />
+  <div>
+    <master-modal 
+      v-model="modal.show"
+      :title="this.isRestore ? `${this.restore.title} - ${this.itemLabel}` : `${this.deletePermanently.title} - ${this.itemLabel}`" 
+      :loading="modal.loading"
+    >
+      <div>
+        <p v-if="item" class="q-my-md">
+          Register: {{ item.id }} - {{ itemLabel }}
+        </p>
+        <dynamic-field :field="this.restore" /> 
+        <dynamic-field :field="this.deletePermanently" /> 
+      </div>
+    </master-modal>    
+    <!-- banner info -->
+    <dynamic-field :field="info" />
+    <!-- crud component-->
+    <component v-if="dynamicCrud" :is="dynamicCrud" ref="componentCrudData" />
   </div>
 </template>
 <script>
@@ -13,9 +29,73 @@ mounted(){
 data() {
   return {
     crudId: this.$uid(),
-    params: {},
-    modules: null, 
-    cruds: []
+    action: null, 
+    item: null,
+    modal: {
+      show: false,
+      loading: false,       
+    },    
+    info: {
+      type: 'banner',
+      props: {
+        color: 'info',
+        icon: 'fas fa-exclamation-triangle',
+        message: 'This is the recycle bin, When you delete a register, it goes to this Recycle Bin as a temporary file. You can restore It or delete It permanently from its options.',        
+      }
+    },
+    restore: {
+      title: 'Restore register',
+      type: 'banner',
+      props: {
+        color: 'green',
+        icon: 'fas fa-exclamation-triangle',
+        message: 'Do you want to restore this register?, this action will move the register to its original table',
+        actions: [                    
+          
+          {
+            props: {
+              label: 'Restore this register',
+              color: 'green'
+            },
+            action: () => {
+                console.log('restore the item')
+            }
+          }
+        ]
+      }
+    },
+    deletePermanently: {
+      title: 'Delete Permanently',
+      type: 'banner',
+      props: {
+        color: 'negative',
+        icon: 'fas fa-exclamation-triangle',
+        message: 'Do you want to delete Permanently this register?, Once you delete this register, there is no going back. Please be certain.',
+        actions: [
+          {
+            props: {
+              label: 'cancel',
+              color: 'grey-4', 
+              textColor: 'black',
+              class: 'q-mx-md'
+
+            },
+            action: () => {
+              this.modal.show = false
+            }
+          },
+          {
+            props: {
+              label: 'Delete this register permanently',
+              color: 'negative'
+            },
+            action: () => {
+                console.log('delete the item')
+            }
+          }
+        ]
+      }
+    },   
   }
 },
 computed: {  
@@ -28,7 +108,6 @@ computed: {
     if (this.isRecycle) {    
       if(this.getModule){
         try {
-          //return require(`@imagina/qblog/_crud/${this.getModule}`).default
           return dynamicCrud.importCrud(this.getModule[0], this.getModule[1])          
         } catch(e) {
           console.log(e)
@@ -38,8 +117,14 @@ computed: {
       return false
     }
   },
+  isRestore(){
+    return this.action == 'restore'
+  },
   isRecycle(){
     return this.$route.name == 'app.recycle'
+  },
+  itemLabel(){    
+    return this.item ? this.item.name || this.item.title || this.item.userName || '' : ''
   },
   getModule(){        
     const params = decodeURI(window.location).split('?')
@@ -71,7 +156,10 @@ computed: {
           label: 'Restore item',
           //vIf: ,
           action: (item) => {
-          this.$emit('restore', item)
+            this.item = item
+            this.action = 'restore'
+            this.modal.show = true
+            this.$emit('restore', item)
           }
         },
         {//restore item action
@@ -80,7 +168,10 @@ computed: {
           label: 'Delete permanently',
           //vIf: ,
           action: (item) => {
-          this.$emit('deletePermanently', item)
+            this.item = item
+            this.action = 'delete'
+            this.modal.show = true          
+            this.$emit('deletePermanently', item)
           }
         },
       ]
