3f570a4735813cb626dc64c55d38f9d190c73921
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2024-07-15 18:10:52 -0500

    -- remove plugin

--- _plugins/dynamicCrud.js
+++
@@ -1,9 +0,0 @@
-export default {
-  importCrud(moduleName, view){
-    try {
-      return require(`@imagina/${moduleName}/_crud/${view}`).default
-    } catch (e) {
-      console.log(e)      
-    }      
-  }
-}
