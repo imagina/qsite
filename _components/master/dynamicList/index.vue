f7fe2eace4f8bbd9b07fae8d59579a00240e0f25
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2024-07-29 12:03:21 -0500

    + added dynamicList component folder

---
+++ _components/master/dynamicList/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="pageId">
+  </div>
+</template>
+<script lang="ts">
+import {defineComponent} from 'vue'
+import controller from 'modules/qsite/_components/v3/demo/controller'
+
+export default defineComponent({
+  props: {},
+  components: {},
+  setup(props, {emit}) {
+    return controller(props, emit)
+  }
+})
+</script>
+<style lang="scss">
+</style>
