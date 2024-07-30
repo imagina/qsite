bf8ddfe287211b00a28e47277ed84185e8c457a7
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2024-07-30 11:16:09 -0500

    + added getData service

--- _components/master/dynamicList/controller.ts
+++ _components/master/dynamicList/controller.ts
@@ -1,6 +1,6 @@
 import {computed, reactive, ref, onMounted, toRefs, watch, getCurrentInstance} from "vue";
-import service from 'modules/qsite/_components/v3/demo/services'
-import store from 'modules/qsite/_components/v3/demo/store'
+
+import services from "modules/qsite/_components/master/dynamicList/services";
 
 export default function controller(props: any, emit: any) {
   const proxy = getCurrentInstance()!.appContext.config.globalProperties
@@ -13,6 +13,8 @@ export default function controller(props: any, emit: any) {
   // States
   const state = reactive({
     // Key: Default Value
+    loading: false,
+    rows: [],
   })
 
   // Computed
@@ -23,10 +25,24 @@ export default function controller(props: any, emit: any) {
   // Methods
   const methods = {
     // methodKey: () => {}
+    init(){
+      methods.getData(props.apiRoute, true)
+    },
+    async getData(apiRoute, refresh = false, params = {}){
+      state.loading = true
+      services.getData(apiRoute, refresh, params).then((response) => {
+        if(response?.data){
+          state.rows = response.data
+          state.loading = false
+          emit('dataLoaded', response.data)
+        }
+      })
+    },
   }
 
   // Mounted
   onMounted(() => {
+    methods.init()
   })
 
   // Watch
@@ -34,5 +50,5 @@ export default function controller(props: any, emit: any) {
   //
   // }, {deep: true})
 
-  return {...refs, ...(toRefs(state)), ...computeds, ...methods, store}
+  return {...refs, ...(toRefs(state)), ...computeds, ...methods}
 }
--- _components/master/dynamicList/index.vue
+++ _components/master/dynamicList/index.vue
@@ -5,26 +5,27 @@
      :columns="columns"
      :rows="rows"
      :actions="actions"
+     :loading="loading"
     />
   </div>
 </template>
 <script lang="ts">
 import {defineComponent} from 'vue'
-//import controller from 'modules/qsite/_components/v3/demo/controller'
+import controller from 'modules/qsite/_components/master/dynamicList/controller'
 import dynamicTable from 'modules/qsite/_components/master/dynamicTable'
 
 export default defineComponent({
-  props: {    
+  props: {
+    apiRoute: { default: ''},
     title: { default: ''},
     columns: {default: []},
-    rows: {default: []},
     actions: {default: []}
   },
   components: {
     dynamicTable
   },
   setup(props, {emit}) {
-    //return controller(props, emit)
+    return controller(props, emit)
   }
 })
 </script>
