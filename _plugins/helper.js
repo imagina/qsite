07c0b88b385837787ca5e85b27ffd7cd578590e1
WilmerAce <wilmerc@imaginacolombia.com>
2024-08-22 15:20:59 -0500

    +FIX: device new code

--- _plugins/helper.js
+++ _plugins/helper.js
@@ -635,37 +635,12 @@ class Helper {
   }
   detectDevice() {
     const userAgent = navigator.userAgent.toLowerCase();
-
-    let device = "Unknown";
-    let version = "Unknown";
-
-    if (/iphone|ipad|ipod/.test(userAgent)) {
-      device = "iPhone/iPad/iPod";
-      const versionMatch = userAgent.match(/os (\d+_\d+)/);
-      if (versionMatch) {
-        version = versionMatch[1].replace('_', '.');
-      }
-    } else if (/android/.test(userAgent)) {
-      device = "Android";
-      const versionMatch = userAgent.match(/android (\d+(\.\d+)?)/);
-      if (versionMatch) {
-        version = versionMatch[1];
-      }
-    } else if (/macintosh|mac os x/.test(userAgent)) {
-      device = "Mac";
-      const versionMatch = userAgent.match(/mac os x (\d+[_\d]+)/);
-      if (versionMatch) {
-        version = versionMatch[1].replace(/_/g, '.');
-      }
-    } else if (/windows/.test(userAgent)) {
-      device = "Windows";
-      const versionMatch = userAgent.match(/windows nt (\d+(\.\d+)?)/);
-      if (versionMatch) {
-        version = versionMatch[1];
-      }
+    const regex = /\(([^)]+)\)/;
+    const match = userAgent.match(regex);
+    if (match && match[1]) {
+      return match[1];
     }
-
-      return `${device} ${version}`;
+    return '';
    }
   }
 
