9bf57863e242a8d3cdb904c989515b0295bc9d5e
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2024-11-01 12:55:51 -0500

    + ident

--- _components/master/dynamicFilter/components/filterChip.vue
+++ _components/master/dynamicFilter/components/filterChip.vue
@@ -1,32 +1,32 @@
 <script setup>
-import { defineProps, defineEmits } from 'vue'
+	import { defineProps, defineEmits } from 'vue'
 
-defineEmits(['remove'])
-defineProps({
-    summary: { 
-        type: Object, 
-        required: true 
-    },
-    props: {
-        type: Object,
-        default: () => {
-            return {}
-        }       
-    },
-})
+	defineEmits(['remove'])
+	defineProps({
+		summary: {
+			type: Object,
+			required: true
+		},
+		props: {
+			type: Object,
+			default: () => {
+				return {}
+			}
+		},
+	})
 </script>
 <template>
-    <template v-for="(item, itemKey) in summary"  :key="itemKey" >
-        <q-chip
-            v-if="item.label !== ''"
-            class="tw-pr-5 tw-bg-gray-100" 
-            text-color="primary"
-            v-bind="props"
-						@remove="$emit('remove', itemKey)"
-        >
-            <span class="text-weight-bold">{{ item.label }}</span>
-            &nbsp;
-            <span class="tw-mr-1">{{ item.option }}</span>
-        </q-chip>
-    </template>
+	<template v-for="(item, itemKey) in summary"  :key="itemKey" >
+		<q-chip
+			v-if="item.label !== ''"
+			class="tw-pr-5 tw-bg-gray-100"
+			text-color="primary"
+			v-bind="props"
+			@remove="$emit('remove', itemKey)"
+		>
+			<span class="text-weight-bold">{{ item.label }}</span>
+			&nbsp;
+			<span class="tw-mr-1">{{ item.option }}</span>
+		</q-chip>
+	</template>
 </template>
