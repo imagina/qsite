fef8f06ee6f6e91a294ca8fdb7edc3f53527c667
nicolas-montealegre <nmontealegre@contractor.renuityhome.com>
2024-11-22 19:00:27 -0500

    Added: feature select All and quantityMultiple

--- _i18n/es/index.js
+++ _i18n/es/index.js
@@ -482,9 +482,9 @@ export default {
         'dispatch': 'Despachar',
         'bulkPermissions': 'Permisos masivos',
         'entityPermissions': 'Permisos de entidad',
-        'cancelled': 'Cancelado',
         'stackTrace': 'Seguimiento de pila',
-        "appliedFilters": "Tienes filtros aplicados, haz clic aquí para abrir."
+        "appliedFilters": "Tienes filtros aplicados, haz clic aquí para abrir.",
+        'selectAll': 'Select All'
       },
       'message': {
         'requestAdded': 'Petición agregada a la cola sin conexión',
