cabc3000f2dda7baf51d2d2dee126a0e04d0da12
msolanorenuity <msolano@contractor.renuityhome.com>
2025-02-24 17:45:12 -0500

    updated btnMenu component to do the route-links

--- _components/master/btnMenu.vue
+++ _components/master/btnMenu.vue
@@ -11,7 +11,6 @@
     v-if="showBtnMenu"
   >
     <q-menu
-      max-width="200px"
       class="btn-menu-component__menu tw-shadow-lg"
       anchor="bottom right"
       self="top right"
@@ -61,15 +60,18 @@ export default {
           if (item.toRoute) item.props.href = item.toRoute;
 
           //Instance vue route redirect
-          if (item.route)
+          if (item.route) {
             item.props.to = {
               name: item.route,
-              params: this.$clone(this.actionData || {}),
+              params: this.$helper.getRouteParams(
+                this.$router.getRoutes().find(route => route.name === item.route),
+                this.actionData
+              ),
             };
-
+          }
           // Formatting all instances
           if (item.format)
-            item = { ...item, ...(item.format(this.actionData) || {}) };
+            item = { ...item, ...(item.format(this.actionData, item) || {}) };
 
           //Return item
           return item;
@@ -93,17 +95,20 @@ export default {
     },
   },
 };
-</script >
+</script>
 <style lang="scss">
 .btn-menu-component__menu {
   padding: 8px;
   min-width: auto;
   border-radius: 8px;
+
   .q-list {
     min-width: 150px;
   }
+
   .q-item {
     padding: 8px;
+
     .q-icon {
       margin-right: 10px;
     }
